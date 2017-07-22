7b044f2ea687bb992e58da262ede2d4bcfb5fcbd
Dan Parkes <daniel.parkes901@gmail.com>
2017-07-22 19:47:18 +0100

    rearranged buttons and added from obsolete files

--- src/components/ButtonsSetNumbers.js
+++ src/components/ButtonsSetNumbers.js
@@ -7,18 +7,31 @@ class ButtonsSetNumbers extends React.Component {
     render() {
         return (
             <div>
-                <h2>These are the basic numbers</h2>
-                <button type="button" onClick={() => this.props.appendValue('.')}>.</button>
-                <button type="button" onClick={() => this.props.appendValue('0')}>0</button>
-                <button type="button" onClick={() => this.props.appendValue('1')}>1</button>
-                <button type="button" onClick={() => this.props.appendValue('2')}>2</button>
-                <button type="button" onClick={() => this.props.appendValue('3')}>3</button>
-                <button type="button" onClick={() => this.props.appendValue('4')}>4</button>
-                <button type="button" onClick={() => this.props.appendValue('5')}>5</button>
-                <button type="button" onClick={() => this.props.appendValue('6')}>6</button>
-                <button type="button" onClick={() => this.props.appendValue('7')}>7</button>
-                <button type="button" onClick={() => this.props.appendValue('8')}>8</button>
-                <button type="button" onClick={() => this.props.appendValue('9')}>9</button>
+                <div style={this.style}>
+                    <button style ={this.numberStyle} type="button" onClick={() => this.props.appendValue('7')}>7</button>
+                    <button style ={this.numberStyle} type="button" onClick={() => this.props.appendValue('8')}>8</button>
+                    <button style ={this.numberStyle} type="button" onClick={() => this.props.appendValue('9')}>9</button>
+                    <button style ={this.funkyStyle} type="button" onClick={() => {this.props.setOperation('*');}}>X</button>
+
+                </div>
+                <div style={this.style}>
+                    <button style ={this.numberStyle} type="button" onClick={() => this.props.appendValue('4')}>4</button>
+                    <button style ={this.numberStyle} type="button" onClick={() => this.props.appendValue('5')}>5</button>
+                    <button style ={this.numberStyle} type="button" onClick={() => this.props.appendValue('6')}>6</button>
+                    <button style ={this.funkyStyle} type="button" onClick={() => {this.props.setOperation('-');}}>-</button>
+                    
+                </div>
+                <div style={this.style}>
+                    <button style ={this.numberStyle} type="button" onClick={() => this.props.appendValue('1')}>1</button>
+                    <button style ={this.numberStyle} type="button" onClick={() => this.props.appendValue('2')}>2</button>
+                    <button style ={this.numberStyle} type="button" onClick={() => this.props.appendValue('3')}>3</button>
+                    <button style ={this.funkyStyle} type="button" onClick={() => {this.props.setOperation('+');}}>+</button>
+                </div>
+                <div style={this.style}>
+                    <button style ={this.zeroStyle} type="button" onClick={() => this.props.appendValue('0')}>0</button>
+                    <button style ={this.numberStyle} type="button" onClick={() => this.props.appendValue('.')}>.</button>
+                    <button type="button" style={this.funkyStyle} onClick={() => {this.props.executeOperation();}}>=</button>
+                </div>
             </div>
         );
     }
