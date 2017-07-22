6511d956105e7c05afab87e7ef4fcfc5968e453a
Dan Parkes <daniel.parkes901@gmail.com>
2017-07-22 19:49:04 +0100

    removed deleted file and rearranged view

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,20 +1,18 @@
 import React from 'react';
 
 import ButtonsSetNumbers from './ButtonsSetNumbers';
-import ButtonsSetFunction from './ButtonsSetFunction';
 import ButtonsSetEquation from './ButtonsSetEquation';
 import Display from './Display';
-import style from './index.css';
+// import style from './index.css';
 
 class Calculator extends React.Component {
     render() {
         return (
-            <div style={{border:'1px solid red', width:'300px'}}>
-                <h1 style={{color:'green'}}>Shaun the calculator</h1>
+            <div style={{border:'1px solid black', width:'300px'}}>
+                <h1 style={{color:'red'}}>Shaun the calculator</h1>
                 <Display/>
-                <ButtonsSetNumbers/>
-                <ButtonsSetFunction/>
                 <ButtonsSetEquation/>
+                <ButtonsSetNumbers/>
             </div>
         );
     }
