cbe5ad4a835298ba58639df1f1e5e3c302ea9c9e
Dan Parkes <daniel.parkes901@gmail.com>
2017-09-07 14:14:26 +0100

    operation is changed correctly

--- spec/helperFuncs.spec.js
+++ spec/helperFuncs.spec.js
@@ -3,13 +3,44 @@ const {
 } = require('chai');
 
 const {
-    operation,
-    updateInput
+    operation, 
+    updateInput,
+    calculateTotal,
+    immediateExecute
 } = require('../src/redux/helperFuncs');
 
-describe('helper functions - operation and updateInput', () => {
+describe('helper functions - operation, updateInput, calculateTotal and immediateExecute', () => {
     it('exists', () => {
         expect(operation).to.be.a('function');
         expect(updateInput).to.be.a('function');
+        expect(calculateTotal).to.be.a('function');
+        expect(immediateExecute).to.be.a('function');
+    });
+    it('operation changes operation correctly', () => {
+        const state = {
+            runningTotal: '0',
+            operation: null,
+            operand: '0',
+        };
+        const op = {
+            type: 'SET_OPERATION',
+            operation: '*'
+        };
+        const op2 = {
+            type: 'SET_OPERATION',
+            operation: '/'
+        };
+        const op3 = {
+            type: 'SET_OPERATION',
+            operation: '+'
+        };
+        const op4 = {
+            type: 'SET_OPERATION',
+            operation: '-'
+        };
+        expect(operation(state, op).operation).to.equal('*');
+        expect(operation(state, op2).operation).to.equal('/');
+        expect(operation(state, op3).operation).to.equal('+');
+        expect(operation(state, op4).operation).to.equal('-');
     });
 });
