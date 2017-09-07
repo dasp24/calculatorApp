c7dd45ba17a2e38d1bada2b8c35bc4370a252a62
Dan Parkes <daniel.parkes901@gmail.com>
2017-09-07 14:34:58 +0100

    tests for numbers being added correctly

--- spec/helperFuncs.spec.js
+++ spec/helperFuncs.spec.js
@@ -3,7 +3,7 @@ const {
 } = require('chai');
 
 const {
-    operation, 
+    operation,
     updateInput,
     calculateTotal,
     immediateExecute
@@ -16,6 +16,9 @@ describe('helper functions - operation, updateInput, calculateTotal and immediat
         expect(calculateTotal).to.be.a('function');
         expect(immediateExecute).to.be.a('function');
     });
+});
+
+describe('operation', () => {
     it('operation changes operation correctly', () => {
         const state = {
             runningTotal: '0',
@@ -42,5 +45,52 @@ describe('helper functions - operation, updateInput, calculateTotal and immediat
         expect(operation(state, op2).operation).to.equal('/');
         expect(operation(state, op3).operation).to.equal('+');
         expect(operation(state, op4).operation).to.equal('-');
+    });
+    it('doesnt change operation if standard values arent sent', () => {
+        const state = {
+            runningTotal: '0',
+            operation: null,
+            operand: '0',
+        };
+        const op = {
+            type: 'SET_OPERATION',
+            operation: 'anything else'
+        };
+        expect(operation(state, op).operation).to.equal(null);
+    });
+});
+
+describe('updateInput', () => {
+    it('updates running total correctly - numbers', () => {
+        const state = {
+            runningTotal: '0',
+            operation: null,
+            operand: '0'
+        };
+
+        const op = {
+            type: 'APPEND_VALUE',
+            value: '1'
+        };
+        const op2 = {
+            type: 'APPEND_VALUE',
+            value: '9'
+        };
+        expect(updateInput(state, op).runningTotal).to.equal('1');
+        expect(updateInput(state, op).runningTotal).to.equal('11');        
+        expect(updateInput(state, op2).runningTotal).to.equal('119');
+    });
+    it('updates running total correctly - .', () => {
+        const state = {
+            runningTotal: '0',
+            operation: null,
+            operand: '0'
+        };
+        const op = {
+            type: 'APPEND_VALUE',
+            value: '.'
+        };
+        expect(updateInput(state, op).runningTotal).to.equal('.');
+        
     });
 });
